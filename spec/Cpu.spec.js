82c9e3c73009e27b64b8976a19c087bf881729da
Manuel Kiessling <manuel@kiessling.net>
2012-04-17 10:42:31 +0200

    specced IFB

--- spec/Cpu.spec.js
+++ spec/Cpu.spec.js
@@ -153,4 +153,102 @@ define(['../lib/Memory', '../lib/Cpu'], function(Memory, Cpu) {
     });
   });
 
+  describe('IFN', function() {
+    it('skips the next instruction if values are equal', function() {
+      var cpu = setupCpu([
+        0x8c01,   // SET A, 3
+        0x8c11,   // SET B, 3
+        0x040d,   // IFN A, B
+        0x9c11,   // SET B, 7
+        0xa401    // SET A, 9
+      ]);
+      for (var i = 0; i < 5; i++) {
+        cpu.step();
+      }
+      expect(cpu.registers[0x00]).toEqual(0x0009);
+      expect(cpu.registers[0x01]).toEqual(0x0003);
+    });
+
+    it('executes the next instruction if values are not equal', function() {
+      var cpu = setupCpu([
+        0x8c01,   // SET A, 3
+        0x9011,   // SET B, 4
+        0x040d,   // IFN A, B
+        0x9c11,   // SET B, 7
+        0xa401    // SET A, 9
+      ]);
+      for (var i = 0; i < 5; i++) {
+        cpu.step();
+      }
+      expect(cpu.registers[0x00]).toEqual(0x0009);
+      expect(cpu.registers[0x01]).toEqual(0x0007);
+    });
+  });
+
+
+  describe('IFG', function() {
+    it('skips the next instruction if a<=b', function() {
+      var cpu = setupCpu([
+        0x8c01,   // SET A, 3
+        0x8c11,   // SET B, 3
+        0x040e,   // IFG A, B
+        0x9c11,   // SET B, 7
+        0xa401    // SET A, 9
+      ]);
+      for (var i = 0; i < 5; i++) {
+        cpu.step();
+      }
+      expect(cpu.registers[0x00]).toEqual(0x0009);
+      expect(cpu.registers[0x01]).toEqual(0x0003);
+    });
+
+    it('executes the next instruction if a>b', function() {
+      var cpu = setupCpu([
+        0x9401,   // SET A, 5
+        0x9011,   // SET B, 4
+        0x040e,   // IFG A, B
+        0x9c11,   // SET B, 7
+        0xa401    // SET A, 9
+      ]);
+      for (var i = 0; i < 5; i++) {
+        cpu.step();
+      }
+      expect(cpu.registers[0x00]).toEqual(0x0009);
+      expect(cpu.registers[0x01]).toEqual(0x0007);
+    });
+  });
+
+
+  describe('IFB', function() {
+    it('skips the next instruction if (a&b)==0', function() {
+      var cpu = setupCpu([
+        0x8001,   // SET A, 0
+        0x8c11,   // SET B, 3
+        0x040f,   // IFB A, B
+        0x9c11,   // SET B, 7
+        0xa401    // SET A, 9
+      ]);
+      for (var i = 0; i < 5; i++) {
+        cpu.step();
+      }
+      expect(cpu.registers[0x00]).toEqual(0x0009);
+      expect(cpu.registers[0x01]).toEqual(0x0003);
+    });
+
+    it('executes the next instruction if (a&b)!=0', function() {
+      var cpu = setupCpu([
+        0x9401,   // SET A, 5
+        0x9011,   // SET B, 4
+        0x040f,   // IFB A, B
+        0x9c11,   // SET B, 7
+        0xa401    // SET A, 9
+      ]);
+      for (var i = 0; i < 5; i++) {
+        cpu.step();
+      }
+      expect(cpu.registers[0x00]).toEqual(0x0009);
+      expect(cpu.registers[0x01]).toEqual(0x0007);
+    });
+  });
+
 });
