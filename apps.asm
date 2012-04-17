2606510ee94c4c73e10ea88e8de501e790d4bce8
Manuel Kiessling <manuel@kiessling.net>
2012-04-17 09:02:03 +0200

    Implemented MOD, SHL, SHR, AND, BOR, XOR, untested

--- apps.asm
+++ apps.asm
@@ -45,3 +45,51 @@ DIV A, B
 d001
 9411
 0405
+
+
+
+
+
+
+; Try some basic stuff
+                      SET A, 0x30              ; 7c01 0030
+                      SET [0x1000], 0x20       ; 7de1 1000 0020
+                      SUB A, [0x1000]          ; 7803 1000
+                      IFN A, 0x10              ; c00d
+                         SET PC, crash         ; 7dc1 001a [*]
+
+        ; Do a loopy thing
+                      SET I, 10                ; a861
+                      SET A, 0x2000            ; 7c01 2000
+        :loop         SET [0x2000+I], [A]      ; 2161 2000
+                      SUB I, 1                 ; 8463
+                      IFN I, 0                 ; 806d
+                         SET PC, loop          ; 7dc1 000d [*]
+
+        ; Call a subroutine
+                      SET X, 0x4               ; 9031
+; Hang forever. X should now be 0x40 if everything went right.
+        :crash        SET PC, crash            ; 7dc1 001a [*]
+
+7c01
+0030
+7de1
+1000
+0020
+7803
+1000
+c00d
+7dc1
+0014
+a861
+7c01
+2000
+2161
+2000
+8463
+806d
+7dc1
+000d
+9031
+7dc1
+0014
