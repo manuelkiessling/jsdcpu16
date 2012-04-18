a55132516109f2b7b120d3cb67e216fcb582ef91
Manuel Kiessling <manuel@kiessling.net>
2012-04-18 11:58:07 +0200

    changed to 32x12 console according to http://0x10cwiki.com/wiki/Video_RAM

--- app/console.js
+++ app/console.js
@@ -5,7 +5,7 @@ define([], function() {
   var draw = function(element, memory, startAddress) {
     var content = '';
     var string;
-    for (var row = 0; row < 16; row++) {
+    for (var row = 0; row < 12; row++) {
       for (var col = 0; col < 32; col++) {
         string = String.fromCharCode(memory.read(startAddress + (row * 32) + col));
         if (string == String.fromCharCode(0)) {
